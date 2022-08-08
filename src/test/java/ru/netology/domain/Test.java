1e1e10be14961f734f5734a3a5b8d181494078dc
Екатерина Чернова <hd106906bqwerty@gmail.com>
2022-08-08 16:49:01 +0300

    Appveyor.yml was modified

--- src/test/java/ru/netology/domain/Test.java
+++ src/test/java/ru/netology/domain/Test.java
@@ -2,12 +2,30 @@ package ru.netology.domain;
 
 import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Configuration;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
 import java.time.Duration;
 
 import static com.codeborne.selenide.Selenide.*;
 
 public class Test {
+    private WebDriver driver;
+
+    @BeforeAll
+    public static void setUpAll() {
+        WebDriverManager.chromedriver().setup();
+    }
+
+    @BeforeEach
+    public void setUp() {
+        driver = new ChromeDriver();
+
+
+    }
     @org.junit.jupiter.api.Test
     public void myTest() {
         Configuration.holdBrowserOpen = true;
