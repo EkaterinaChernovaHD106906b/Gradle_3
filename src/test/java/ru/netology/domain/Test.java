1aebefdf069b3ba133836b63ce53d398681e33c9
Екатерина Чернова <hd106906bqwerty@gmail.com>
2022-08-11 15:28:50 +0300

    Test was modified

--- src/test/java/ru/netology/domain/Test.java
+++ src/test/java/ru/netology/domain/Test.java
@@ -4,26 +4,36 @@ import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Configuration;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.jupiter.api.BeforeAll;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 
 import java.time.Duration;
+import java.time.LocalDate;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
 
 import static com.codeborne.selenide.Selenide.*;
 
 public class Test {
+    public String generateDate(int days) {
+        return LocalDate.now().plusDays(3).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
+    }
 
+    String planningDate = generateDate(3);
 
     @org.junit.jupiter.api.Test
     public void myTest() {
         Configuration.holdBrowserOpen = true;
         open("http://localhost:9999");
         $("[data-test-id=\"city\"] input").setValue("Нальчик");
-        $("[data-test-id='date'] input ").setValue("11.08.2022");
+        $("[data-test-id='date'] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.BACK_SPACE);
+        $("[data-test-id='date'] input ").setValue(planningDate);
         $("[data-test-id='name'] input").setValue("Алексей Орлов-Чесменский");
         $("[data-test-id='phone'] input").setValue("+76540785412");
         $("[data-test-id= 'agreement']").click();
         $x("//*[text()='Забронировать']").click();
         $("[data-test-id='notification']").shouldBe(Condition.visible, Duration.ofSeconds(15));
+        $("[data-test-id='notification']").shouldHave(Condition.text("Успешно! Встреча успешно забронирована на " + planningDate), Duration.ofSeconds(15)).shouldBe(Condition.visible);
 
     }
 }
